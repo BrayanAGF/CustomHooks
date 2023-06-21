4a9329ac020554abdb6298c6558e6f5cfac84c3a
Brayan <mrbrayan.bagf@gmail.com>
2023-06-21 08:49:56 -0600

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,49 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../Components/08-useReducer/todoReducer";
+
+export const useTodo = ( initialState = [] ) => {
+
+    const init = () => {
+        return JSON.parse( localStorage.getItem('todos') || []);
+    }
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+    
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify( todos ));
+    }, [todos]);
+    
+
+    const handleNewTodo = ( todo ) => {
+        const action = {
+            type: 'Create',
+            payload: todo
+        }
+
+        dispatch( action );
+    }
+
+    const handleDeleteTodo = ( id ) => {
+        dispatch( {
+            type: 'Delete',
+            payload: id
+        } );
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatch( {
+            type: 'Toggle',
+            payload: id
+        } );
+    } 
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo
+    }
+
+}
